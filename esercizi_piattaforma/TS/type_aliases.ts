6a94fadd330924dbedf631398a84d1d8ffd6bb60
mirkoEscobedo <escobedomirko@gmail.com>
2024-11-10 15:45:54 +0100

    first 4 chapters of exercises

---
+++ esercizi_piattaforma/TS/type_aliases.ts
@@ -0,0 +1,9 @@
+// Create a Country type alias for the string type.
+// Change the type for each variable to use the Country type.
+type Country = string;
+
+const country1: Country = 'Nigeria';
+
+const country2: Country = 'Italy';
+
+const country3: Country = 'China';
