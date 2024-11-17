2f7e4613aead191543421c1182d9aa182f36815a
mirkoEscobedo <escobedomirko@gmail.com>
2024-11-17 21:12:32 +0100

    promise chaining

---
+++ esercizi_piattaforma/NODE/promises_lucky_draw/index.js
@@ -0,0 +1,27 @@
+function luckyDraw(player) {
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
+
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
+    });
+  });
+}
+
+luckyDraw('Joe')
+  .then((result) => {
+    console.log(result);
+  })
+  .then(() => luckyDraw('Caroline'))
+  .then((result) => {
+    console.log(result);
+  })
+  .then(() => luckyDraw('Sabrina'))
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => console.error(err));
