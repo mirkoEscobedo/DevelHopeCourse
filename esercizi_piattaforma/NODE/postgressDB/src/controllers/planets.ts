330a572e415db811e6fb93b899b64cc4432926a7
mirkoEscobedo <escobedomirko@gmail.com>
2024-11-22 16:18:20 +0100

    ambush.jpeg

--- esercizi_piattaforma/NODE/postgressDB/src/controllers/planets.ts
+++ esercizi_piattaforma/NODE/postgressDB/src/controllers/planets.ts
@@ -1,3 +1,37 @@
+//Ambush.txt YO IS THIS WRONG? <3
+// import fs from 'node:fs';
+// import path from 'node:path';
+// import archiver from 'archiver';
+
+// export async function createZipFromFiles(
+//   filePaths: string[],
+//   outputFilePath: string
+// ): Promise<string> {
+//   return new Promise((resolve, reject) => {
+//     const output = fs.createWriteStream(outputFilePath);
+//     const archive = archiver('zip', { zlib: { level: 9 } });
+
+//     output.on('close', () => {
+//       console.log(`${archive.pointer()} total bytes`);
+//       console.log('Zip archive created successfully.');
+//       resolve(outputFilePath);
+//     });
+
+//     archive.on('error', (err) => {
+//       console.error('Error creating ZIP archive', err);
+//       reject(err);
+//     });
+
+//     archive.pipe(output);
+
+//     filePaths.forEach((filePath) => {
+//       archive.file(filePath, { name: path.basename(filePath) });
+//     });
+//     console.log('finilizing archive...');
+//     archive.finalize();
+//     console.log('Archive finalized successfully.');
+//   });
+// }
 import { Request, Response } from 'express';
 import Joi from 'joi';
 import pgPromise from 'pg-promise';
