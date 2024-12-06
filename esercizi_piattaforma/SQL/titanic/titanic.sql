e7cfe0df55579230ae9d895e9dd601812d0be24a
mirkoEscobedo <escobedomirko@gmail.com>
2024-12-06 15:43:34 +0100

    fixed fucked up query what a pepega

--- esercizi_piattaforma/SQL/titanic/titanic.sql
+++ esercizi_piattaforma/SQL/titanic/titanic.sql
@@ -1,6 +1,6 @@
 SELECT * FROM tested WHERE c5 = 'female' AND c6 >= 30;
 
-SELECT * FROM tested WHERE c2 = 0 AND c5 = 'male';
+SELECT AVG(c6) FROM tested WHERE  c2 = 0 AND c5 = 'male';
 
 SELECT * FROM tested WHERE c12 = 'C' AND c10 > 20 AND c10 <50;
 
